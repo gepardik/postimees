cc224b20c27723e5912cf22d9b2877f0e29d006b
Galina Morozova <t030626@mail.com>
2021-06-16 12:10:31 +0300

    fixed page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'postimees',
+    title: 'Postimees',
     htmlAttrs: {
       lang: 'en'
     },
